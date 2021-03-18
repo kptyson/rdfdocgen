a05e5435dc1cf5bfbd4e639de4745550c7e68050
Kevin Tyson <kevin.tyson@gmail.com>
2021-03-18 16:44:28 -0400

    Added URL for project

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='rdfdocgen',
     version='1.0.0',
     packages=['rdfdocgen'],
-    url='',
+    url='https://github.com/kptyson/rdfdocgen',
     license='MIT License',
     author='Kevin Tyson',
     author_email='kevin.tyson@industrialsemantics.com',
